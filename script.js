a64d584033a61dd77a45810748da73ed414724ac
Ayaan Khalif <ayaankhalif@Ayaans-MacBook-Air-4.local>
2023-06-17 13:23:55 +0300

    capitalize-letters assignment - Done

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-// Good Luck 💪🏾
+
+
+function makeCapital(str) {
+    var words = str.split(" ");
+    var capitalizedWords = [];
+  
+    for (var i = 0; i < words.length; i++) {
+      var word = words[i];
+      var firstChar = word[0];
+      var restOfWord = "";
+  
+      for (var j = 1; j < word.length; j++) {
+        restOfWord += word[j];
+      }
+  
+      var capitalizedWord = firstChar.toUpperCase() + restOfWord;
+      capitalizedWords.push(capitalizedWord);
+    }
+  
+    return capitalizedWords.join(" ");
+  }
+
+var input = "my name is ayaan khalif and i live in mogadishu, somalia";
+var output = makeCapital(input);
+console.log(output);
